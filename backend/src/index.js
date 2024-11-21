cd0d1c4362399b35a7f99723a3ab23ea29b17d3d
satyam <satyamremote@gmail.com>
2024-11-21 23:11:58 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { app, server } from "./lib/socket.js";
+
+import cors from "cors";
+import path from "path";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+  connectDB();
+});
---
+++ frontend/src/lib/utils.js
@@ -0,0 +1,8 @@
+export function formatMessageTime(date) {
+  return new Date(date).toLocaleTimeString("en-IN", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+    timeZone: "Asia/Kolkata",
+  });
+}
